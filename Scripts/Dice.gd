e3ee38eef99679dfb006e9e4403cf1991a47cae3
Marcus Kuffner <marcuskuffner3125@gmail.com>
2022-07-16 11:31:30 -0400

    Holding dice added

--- Scripts/Dice.gd
+++ Scripts/Dice.gd
@@ -2,11 +2,12 @@ extends RigidBody2D
 
 export (float) var throw_magnitude = 1100
 export (float) var throw_cooldown_def = 1
-export (int) var dice_stopping_speed = 200
+export (int) var dice_stopping_speed = 400
 var dice_val = 0
 var rolling = false
 var throw_cooldown = 0
 var rng = RandomNumberGenerator.new()
+var attractable = false
 
 func _ready():
 	dice_roll()
@@ -15,22 +16,31 @@ func player_throw():
 	if throw_cooldown <= 0:
 		var force = (get_global_mouse_position() - global_position).normalized() * throw_magnitude
 		apply_central_impulse(force)
+		linear_damp = 1
 		throw_cooldown = throw_cooldown_def
 		rolling = true
 		
 func dice_roll():
 	rng.randomize()
-	dice_val = rng.randi_range(1, 6)
+	dice_val = rng.randi_range(1, 3)
 	print(dice_val)
 
 func _physics_process(delta):
 	if throw_cooldown > 0:
 		throw_cooldown -= delta
+		linear_damp += delta
+	elif rolling:
+		linear_damp += delta * 20
+		
 	if linear_velocity.length() < dice_stopping_speed and rolling and throw_cooldown < 0:
 		rolling = false
 		linear_velocity = Vector2()
 		angular_velocity = 0
 		dice_roll()
+		linear_damp = 1
 
+func _on_Area2D_mouse_entered():
+	attractable = true
 
-
+func _on_Area2D_mouse_exited():
+	attractable = false
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
+onready var Dice = preload("res://Scenes/2D/Dice.tscn")
+
 export (float) var speed = 100
 export (float) var movement_damp = 10
 
 var active_die = null
 var velocity = Vector2()
+var stored_die = null
 
 func _unhandled_input(event):
 	pass
@@ -20,22 +23,35 @@ func get_input():
 		input_velocity.x -= 1
 	if Input.is_action_pressed("Right"):
 		input_velocity.x += 1
-	if Input.is_action_just_released("Click") and active_die != null:
-		active_die.player_throw()
-		
+	if Input.is_action_just_released("Click"):
+		if active_die != null:
+			active_die.player_throw()
+		elif stored_die != null:
+			stored_die = null
+			var dice = Dice.instance()
+			get_parent().add_child(dice)
+			dice.global_position = global_position
+			dice.player_throw()
+			
+			
+	if Input.is_action_just_released("RightClick") and active_die != null:
+		if active_die != null and stored_die == null:
+			stored_die = active_die.dice_val
+			active_die.queue_free()
+			active_die = null
+			
 	return input_velocity.normalized() * speed
 	
 func _physics_process(delta):
 	velocity = ((velocity / movement_damp) * (movement_damp - 1)) + (get_input() / movement_damp)
 	move_and_slide(velocity) 
 
-
 func _on_dice_entered(body):
 	if body.is_in_group("Dice"):
 		active_die = body
 		print("Dice Enter")
 
-func _on_dice_exit(body):
+func _on_dice_exited(body):
 	if active_die == body:
 		active_die = null
 		print("Dice Exit")
